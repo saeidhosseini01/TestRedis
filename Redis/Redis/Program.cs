858dfa8a06bf48b61b195f68f1ee67a4990d55f8
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-04-15 01:32:37 +0430

    redis

---
+++ Redis/Redis/Program.cs
@@ -0,0 +1,24 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redis
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using(IRedisClient client=new RedisClient()) {
+
+                for(int i = 0; i < 5000; i++)
+                {
+                client.Set<string>("test"+i,i.ToString());
+
+                }
+                    }
+        }
+    }
+}
